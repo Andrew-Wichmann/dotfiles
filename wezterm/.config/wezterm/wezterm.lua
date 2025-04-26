9798316388168469f81e138f956b0b2b2eb7c19c
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-26 18:08:01 -0400

    feat: configure catppuccin for wezterm, swaybar, and tmux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false -- Disable the tab bar entirely
 config.window_background_opacity = 0.82
+config.font = wezterm.font("FantasqueSansM Nerd Font Mono")
+--config.window_decorations = "NONE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 12, -- this pushes the bottom of the window to touch the waybar
+  bottom = 0,
+}
 
 return config
